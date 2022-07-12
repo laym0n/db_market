4cb44cbc51988cedfa94fa895372bfbdfef4f69c
k_oa5 <k_oa5@LAPTOP-UMNMJ1CM>
2022-07-13 01:44:56 +0300

    Добавьте файлы проекта.

---
+++ Database1/Procedure1.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].all_products
+AS
+	select p.name, c.name from Product p
+left join dbo.set_category s
+on s.id_product = p.product 
+left join category c
+on c.category = s.id_category
+RETURN 0
